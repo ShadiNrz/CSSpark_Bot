58cb91ff43e078c906a3f086efe1a96576eb2472
shanecranor <shanecranor@gmail.com>
2023-10-31 15:41:09 -0600

    more formatting

--- db_types.py
+++ db_types.py
@@ -1,15 +1,18 @@
 from dataclasses import dataclass
 from typing import Dict, List
 
+
 @dataclass
 class Topic:
     topicName: str
     isExpanded: bool
+
 
 @dataclass
 class User:
     reddit_username: str
     isPublic: bool
     subscribedKeywords: List[Topic]
+
 
 KeywordExpansionDict = Dict[str, List[str]]
