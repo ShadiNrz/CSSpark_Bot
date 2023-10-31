28f1ba0f3d5160e27c4e4a4bd2f11c522d814a19
shanecranor <shanecranor@gmail.com>
2023-10-31 15:54:06 -0600

    fixed formatting for real

--- placeholderData.py
+++ placeholderData.py
@@ -1,7 +1,8 @@
 import json
 from db_types import Topic, User, KeywordExpansionDict
-#JSON placeholder data (probably similar to what we'll get from the database?)
-users_json = '''
+
+# JSON placeholder data (probably similar to what we'll get from the database?)
+users_json = """
 [
     {
         "reddit_username": "123madskillz",
@@ -20,23 +21,24 @@ users_json = '''
         ]
     }
 ]
-'''
+"""
 
-keyword_expansion_json = '''
+keyword_expansion_json = """
 {
     "ai": ["artificial intelligence", "machine learning", "deep learning"],
     "gaming": ["gaming", "video games"],
     "ml": ["machine learning", "deep learning"],
     "robotics": ["robotics", "robots"]
 }
-'''
-
+"""
 # Parsing the JSON string to Python objects
 parsed_users = json.loads(users_json)
 # Converting dictionaries to User and Topic objects
 users = [User(**user_data) for user_data in parsed_users]
 for user in users:
-    user.subscribedKeywords = [Topic(**topic_data) for topic_data in user.subscribedKeywords]
+    user.subscribedKeywords = [
+        Topic(**topic_data) for topic_data in user.subscribedKeywords
+    ]
 
 parsed_keyword_dict: KeywordExpansionDict = json.loads(keyword_expansion_json)
 
