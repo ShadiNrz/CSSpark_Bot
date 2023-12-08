761d83a93c9a2c4b665d0b3155c0f29583e01bab
shanecranor <shanecranor@gmail.com>
2023-12-07 18:34:15 -0700

    print info comments

--- update.sh
+++ update.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+echo fetching from git
 git pull
+echo killing bot
 sudo systemctl stop redditbot.service
+echo starting bot
 sudo systemctl start redditbot.service
+echo printing status
+sudo systemctl status redditbot.service
